55c27dba758f469446d60f8658187453c61361bb
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-29 21:32:29 +0000

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,29 @@
 const request = require('request');
-const arg = process.argv.slice(2)
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`, (error, response, body) => {
-  //console.log('error:', error); // Print the error if one occurred
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    //console.log('error:', error); // Print the error if one occurred
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-  const bodyData = JSON.parse(body);
-  const data = bodyData[0]
-  //console.log(data); // Print the HTML for the Google homepage.
+    const bodyData = JSON.parse(body);
+    const data = bodyData[0];
+    //console.log(data); // Print the HTML for the Google homepage.
 
-  if (data === undefined) {
-    console.log(`The Breed is Not Found`);
-  }
-  if (!error && data != undefined) {
-    console.log(data.name);
-    console.log(data.description);
-  }
-  if (error) {
-    console.log(error);
-  }
+    if (data === undefined) {
+      console.log(`The Breed is Not Found`);
+    }
+    if (!error && data !== undefined) {
+      console.log(data.name);
+      console.log(data.description);
+    }
+    if (error) {
+      console.log(error);
+    }
 
-});
+  });
+};
+
+
+module.exports = {
+  fetchBreedDescription
+};
